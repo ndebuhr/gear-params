2a8531952c734dea28be311c0c398e713c2a238e
Neal DeBuhr <ndebuhr@protonmail.com>
2017-04-25 09:08:08 -0400

    Initial commit

---
+++ get_teeth.c
@@ -0,0 +1,19 @@
+/************************************************
+ * Filename: get_teeth.c
+ * Neal DeBuhr
+ *
+ * Description: Determines the gear teeth needed
+ * to attain a user-specified output speed, given
+ * an input speed and the number of teeth on the
+ * first gear
+ *************************************************/ 
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{  
+
+  
+  
+}
