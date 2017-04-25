a8c1c3f920d0497003b0dfd3e753d899d77a1696
Neal DeBuhr <ndebuhr@protonmail.com>
2017-04-25 13:21:16 -0400

    Create number of teeth calculation function for two gear train case

--- get_teeth.c
+++ get_teeth.c
@@ -13,7 +13,35 @@
 
 int main()
 {  
-
+  float speed_A, speed_B, teeth_A, teeth_B;
+  float whole_speed_B1, whole_speed_B2;
+  int whole_teeth_B1, whole_teeth_B2;
   
+  printf("Enter the speed of the first gear:");
+  scanf("%f",&speed_A);
+
+  printf("Enter the speed of the second gear:");
+  scanf("%f",&speed_B);
+
+  printf("Enter the number of teeth of the first gear:");
+  scanf("%f",&teeth_A);
+
+  teeth_B=(speed_A*teeth_A)/speed_B;
+
+  whole_teeth_B1=(int)teeth_B;
+  whole_speed_B1=(speed_A*teeth_A)/(float)whole_teeth_B1;
+  whole_teeth_B2=(int)teeth_B+1;
+  whole_speed_B2=(speed_A*teeth_A)/(float)whole_teeth_B2;
+
+  if ((float)whole_teeth_B1 != teeth_B)
+    {
+      printf("Closest possible speeds are:\n");
+      printf("%.0f speed with a %d tooth gear\n",whole_speed_B1,whole_teeth_B1);
+      printf("%.0f speed with a %d tooth gear\n",whole_speed_B2,whole_teeth_B2);
+    }
+  else
+    {
+      printf("%.0f speed attainable with a %.0f tooth gear\n",speed_B,teeth_B);
+    }
   
 }
