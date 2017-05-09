2b726e997aa42d9b172d5c2ca01738eab2aa32b6
Neal DeBuhr <ndebuhr@protonmail.com>
2017-05-09 05:53:29 -0400

    Implement small memory optimization

--- gear_params.h
+++ gear_params.h
@@ -2,7 +2,7 @@
 
 #define MODULES 2
 #define MODULE_PARTS 2
-#define MODULE_PART_SIZE 255
+#define MODULE_PART_SIZE 256
 
 char module_choice(void);
 bool module_exists(char user_choice, char module_opt_ptr[MODULES][MODULE_PARTS][MODULE_PART_SIZE], const int num_modules);
