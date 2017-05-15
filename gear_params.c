7087022ad66bf9aaef55d154a2d49ac0fded3545
Neal DeBuhr <ndebuhr@protonmail.com>
2017-05-15 06:39:57 -0400

    Clean up parsing of command-line argument for module preselection

--- gear_params.c
+++ gear_params.c
@@ -18,13 +18,13 @@ int main(int argc, char * argv[])
 
   if (argc>1)
     {
-      if (argv[1][0]=='S' || argv[1][0]=='R') //TODO make this more scalable/clean
-	module_index=argv[1][0];
+      if (argv[1][0]=='-')
+	module_index=module_choice(argv[1][1]);
       else
-	printf("Command line module designation not recognized\n");
+	module_index=module_choice('\0'); //command line module selection incorrectly formatted (ignore it)
     }
   else
-    module_index=module_choice();
+    module_index=module_choice('\0'); //command line module selection not specified
 
   switch (module_index)
     {
@@ -41,7 +41,7 @@ int main(int argc, char * argv[])
   return 0;
 }
 
-char module_choice(void)
+char module_choice(char predet)
 {
   int i;
   char user_choice;
@@ -51,23 +51,35 @@ char module_choice(void)
     {{"S","Spur Gear Train Design"},
      {"R","Rack and Pinion Design"}};
 
-  // Print current modules available
-  printf("Index | Module Name\n");
-  for (i=0; i<MODULES; i++)
-    printf("%4s  | %s\n",module_options[i][0],module_options[i][1]);
+  if (predet=='\0')
+    {
+      // Print current modules available
+      printf("Index | Module Name\n");
+      for (i=0; i<MODULES; i++)
+	printf("%4s  | %s\n",module_options[i][0],module_options[i][1]);
+      printf("Module to run: ");
+      scanf("%c",&user_choice);
+      while (getchar()!='\n'); //clear stdin buffer
+    }
+  else
+    {
+      user_choice=predet;
+    }
   
   do { // TODO clean this up without \n in buffer issue
-    printf("Module to run: ");
-    scanf("%c",&user_choice);
     valid_input=module_exists(user_choice,module_options,MODULES);
     if (valid_input==false)
-      printf("Module index not recognized. Please input one from list.\n");
-    scanf("%c",&char_dump); //dump \n from input buffer
+      {
+	printf("Module index not recognized. Please input one from list.\n");
+	printf("Module to run: ");
+	scanf("%c",&user_choice);
+	while (getchar()!='\n'); //clear stdin buffer
+      }
   } while (valid_input==false);
   
   return user_choice;
 }
-  
+
 bool module_exists(char user_choice, char module_opt_ptr[MODULES][MODULE_PARTS][MODULE_PART_SIZE], const int num_modules)
 {
   int i;
--- gear_params.h
+++ gear_params.h
@@ -4,5 +4,5 @@
 #define MODULE_PARTS 2
 #define MODULE_PART_SIZE 256
 
-char module_choice(void);
+char module_choice(char predet);
 bool module_exists(char user_choice, char module_opt_ptr[MODULES][MODULE_PARTS][MODULE_PART_SIZE], const int num_modules);
