8b4e376e3555193b7daa801e0c44946df171bc54
Neal DeBuhr <ndebuhr@protonmail.com>
2017-06-06 18:28:59 -0400

    Lower program memory requirements by 1536 bytes (extra memory not used)

--- gear_params.h
+++ gear_params.h
@@ -2,7 +2,7 @@
 
 #define MODULES 4
 #define MODULE_PARTS 2
-#define MODULE_PART_SIZE 256
+#define MODULE_PART_SIZE 64
 
 char module_choice(char predet);
 bool module_exists(char user_choice, char module_opt_ptr[MODULES][MODULE_PARTS][MODULE_PART_SIZE], const int num_modules);
