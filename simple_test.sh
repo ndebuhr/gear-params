029b7dd72b1609391bcbe742813e544330509a63
Neal DeBuhr <ndebuhr@protonmail.com>
2017-06-12 11:56:32 -0400

    Integrate test files into quality test script

--- simple_test.sh
+++ simple_test.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+echo "driving.teeth 64" > spur_input.txt
+echo "driving.speed 52" >> spur_input.txt
+echo "driven.teeth ?" >> spur_input.txt
+echo "driven.speed 100" >> spur_input.txt
+
+echo "nVal ?" > rack_input.txt
+echo "pVal 32" >> rack_input.txt
+echo "sVal 100" >> rack_input.txt
+
+echo "dia_pitch ?" > metric_input.txt
+echo "met_module 0.564" >> metric_input.txt
