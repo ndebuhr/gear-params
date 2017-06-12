dcc0852eb3626e83bbe35996228e195c0d0e0b74
Neal DeBuhr <ndebuhr@protonmail.com>
2017-06-12 08:18:03 -0400

    Create input file parsing framework for rack+pinion module

--- rack_pinion.c
+++ rack_pinion.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include <assert.h>
 #include <math.h>
@@ -7,58 +9,38 @@
 
 void rack_pinion(char * input_file)
 {  
-  int i;
   char parameters[NUM_PARAMS][2][64]={{"N","Number of Teeth"},{"P","Pitch Diameter of Pinion"},{"S","Surface Speed"}};
-  bool valid_param=false;
   char unknown_choice;
-  char toss_char;
   float result;
-  
-  printf("Index | Parameter Name\n");    
-  for (i=0;i<NUM_PARAMS;i++)
-    printf("%4s  | %s\n",parameters[i][0],parameters[i][1]);
 
-  while (valid_param==false)
-    {
-      printf("\nParameter to solve for (enter index): ");
-      scanf("%c",&unknown_choice);
-      for (i=0;i<NUM_PARAMS;i++)
-	{
-	  if (unknown_choice==parameters[i][0][0])
-	    valid_param=true;
-	}
-      if (valid_param==false)
-	printf("Invalid index.  Please input an index from list (case sensitive).\n");
-      while (getchar()!='\n'); //clear stdin buffer
-    }
-
-  result=solve_rack_pinion(&unknown_choice);
-  printf("Computed value for %c is %f\n",unknown_choice,result);  
-}
-
-float solve_rack_pinion(char * unknown)
-{
   float nVal;
   float pVal;
   float sVal;
+  
+  if (strcmp(input_file,"Does Not Exist")==0)
+    rack_interactive_parse(parameters,&unknown_choice,&nVal,&pVal,&sVal);
+  else
+    rack_file_parse(input_file,parameters,&unknown_choice,&nVal,&pVal,&sVal);
+
+    
+  result=solve_rack_pinion(&unknown_choice,nVal,pVal,sVal);
+  printf("Computed value for %c is %f\n",unknown_choice,result);  
+}
+
+float solve_rack_pinion(char * unknown, float nVal, float pVal, float sVal)
+{
   float pi=(float)M_PI;
 
   switch (*unknown) {
   case 'N' : //number of teeth
-    sVal=get_s();
-    pVal=get_p();
     nVal=(sVal*1000)/(pi*pVal);
     return nVal;
     break;
   case 'P' : //pitch diameter of pinion
-    sVal=get_s();
-    nVal=get_n();
     pVal=(sVal*1000)/(pi*nVal);
     return pVal;
     break;
   case 'S' : //surface speed
-    pVal=get_p();
-    nVal=get_n();
     sVal=(pi*pVal*nVal)/1000;
     return sVal;
     break;
@@ -96,3 +78,98 @@ float get_s(void)
   assert(S>0);
   return S;
 }
+
+void rack_interactive_parse(char parameters[NUM_PARAMS][2][64],char * unknown_choice, float * nVal, float * pVal, float * sVal)
+{
+  int i;
+  bool valid_param=false;
+  char toss_char;
+  
+  printf("Index | Parameter Name\n");    
+  for (i=0;i<NUM_PARAMS;i++)
+    printf("%4s  | %s\n",parameters[i][0],parameters[i][1]);
+
+  while (valid_param==false)
+    {
+      printf("\nParameter to solve for (enter index): ");
+      scanf("%c",unknown_choice);
+      for (i=0;i<NUM_PARAMS;i++)
+	{
+	  if (*unknown_choice==parameters[i][0][0])
+	    valid_param=true;
+	}
+      if (valid_param==false)
+	printf("Invalid index.  Please input an index from list (case sensitive).\n");
+      while (getchar()!='\n'); //clear stdin buffer
+    }
+  
+  switch (*unknown_choice) {
+  case 'N' : //number of teeth
+    *sVal=get_s();
+    *pVal=get_p();
+    break;
+  case 'P' : //pitch diameter of pinion
+    *sVal=get_s();
+    *nVal=get_n();
+    break;
+  case 'S' : //surface speed
+    *pVal=get_p();
+    *nVal=get_n();
+    break;
+  }
+}
+
+void rack_file_parse(char * input_file, char parameters[NUM_PARAMS][2][64],char * unknown_choice, float * nVal, float * pVal, float * sVal)
+{
+  FILE * fp;
+  char file_str[INPUT_FILE_LINE];
+  char * var_parse;
+  char * val_parse;
+  int i, j;
+  
+  fp = fopen(input_file, "r"); //open file in read only
+  while(fgets(file_str,INPUT_FILE_LINE,fp))
+    {
+      var_parse = (char *)calloc(strlen(file_str)+1,sizeof(char));
+      val_parse = (char *)calloc(strlen(file_str)+1,sizeof(char));
+      i=0;
+      while(file_str[i]!=' ')
+	var_parse[i]=file_str[i++];
+      var_parse[i]='\0';
+      i++;
+      if (file_str[i]=='?')
+	{
+	  if (strcmp(var_parse,"nVal")==0)
+	    *unknown_choice='N';
+	  if (strcmp(var_parse,"pVal")==0)
+	    *unknown_choice='P';
+	  if (strcmp(var_parse,"sVal")==0)
+	    *unknown_choice='S';
+	}
+      else
+	{
+	  j=0;
+	  while(file_str[i]!='\0')
+	    val_parse[j++]=file_str[i++];
+	  val_parse[j]='\0';
+	  if (strcmp(var_parse,"nVal")==0)
+	    {
+	      printf("nVal set to %f\n",atof(val_parse));
+	      *nVal=atof(val_parse);
+	    }
+	  if (strcmp(var_parse,"pVal")==0)
+	    {
+	      printf("pVal set to %f\n",atof(val_parse));
+	      *pVal=atof(val_parse);
+	    }
+	  if (strcmp(var_parse,"sVal")==0)
+	    {
+	      printf("sVal set to %f\n",atof(val_parse));  
+	      *sVal=atof(val_parse);
+	    }
+	}
+      free(var_parse);
+      free(val_parse);
+    }
+  fclose(fp);
+}  
--- rack_pinion.h
+++ rack_pinion.h
@@ -1,7 +1,12 @@
 #define NUM_PARAMS 3
+#define INPUT_FILE_LINE 256
 
 void rack_pinion(char * input_file);
-float solve_rack_pinion(char * unknown);
+float solve_rack_pinion(char * unknown, float nVal, float pVal, float sVal);
 float get_n(void);
 float get_p(void);
 float get_s(void);
+void rack_interactive_parse(char parameters[NUM_PARAMS][2][64],char * unknown_choice, float * nVal, float * pVal, float * sVal);
+void rack_file_parse(char * input_file, char parameters[NUM_PARAMS][2][64],char * unknown_choice, float * nVal, float * pVal, float * sVal);
+
+
