ca683bc98d6ecde557f1f60f20f8fe18693707b6
Neal DeBuhr <ndebuhr@protonmail.com>
2017-06-15 10:31:46 -0400

    Refactoring and clean up

--- gear_params.c
+++ gear_params.c
@@ -12,15 +12,14 @@ int main(int argc, char * argv[])
     char index; //character index for module
     void (*func)(char *); //associated module function
   };
-  struct gear_mod mod_spur_gear, mod_rack_pinion,
-    mod_metric_US;
-  mod_spur_gear.index='S';
-  mod_spur_gear.func=&spur_gears;
-  mod_rack_pinion.index='R';
-  mod_rack_pinion.func=&rack_pinion;
-  mod_metric_US.index='U';
-  mod_metric_US.func=&metric_US;
-  struct gear_mod modules[MODULES]={mod_spur_gear, mod_rack_pinion, mod_metric_US};
+  struct gear_mod spur_mod, rack_mod, metric_mod;
+  spur_mod.index='S';
+  spur_mod.func=&spur_gears;
+  rack_mod.index='R';
+  rack_mod.func=&rack_pinion;
+  metric_mod.index='U';
+  metric_mod.func=&metric_US;
+  struct gear_mod modules[MODULES]={spur_mod, rack_mod, metric_mod};
   char * input_file;
   
   if (argc>1)
@@ -75,7 +74,7 @@ char choose_mod(char predet)
   char user_choice;
   char char_dump;
   bool valid_input;
-  char module_options[MODULES][MODULE_PARTS][MODULE_PART_SIZE]=
+  char mod_options[MODULES][MODULE_PARTS][MODULE_PART_SIZE]=
     {{"S","Spur Gear Train Design"},
      {"R","Rack and Pinion Design"},
      {"U","Unit Conversion Module vs. Diametral Pitch"}};
@@ -85,7 +84,7 @@ char choose_mod(char predet)
       // Print current modules available
       printf("Index | Module Name\n");
       for (i=0; i<MODULES; i++)
-	printf("%4s  | %s\n",module_options[i][0],module_options[i][1]);
+	printf("%4s  | %s\n",mod_options[i][0],mod_options[i][1]);
       printf("Module to run: ");
       scanf("%c",&user_choice);
       while (getchar()!='\n'); //clear stdin buffer
@@ -96,7 +95,7 @@ char choose_mod(char predet)
     }
   
   do {
-    valid_input=module_exists(user_choice,module_options,MODULES);
+    valid_input=module_exists(user_choice,mod_options,MODULES);
     if (valid_input==false)
       {
 	printf("Module index not recognized. Please input one from list.\n");
@@ -109,12 +108,12 @@ char choose_mod(char predet)
   return user_choice;
 }
 
-bool module_exists(char user_choice, char module_opt_ptr[MODULES][MODULE_PARTS][MODULE_PART_SIZE], const int num_modules)
+bool module_exists(char user_choice, char mod_options[MODULES][MODULE_PARTS][MODULE_PART_SIZE], const int num_modules)
 {
   int i;
   for (i=0; i<num_modules; i++)
     {
-      if (user_choice == module_opt_ptr[i][0][0]) //limited to single digit module indexes
+      if (user_choice == mod_options[i][0][0]) //limited to single digit module indexes
 	return true;
     }
   return false;
